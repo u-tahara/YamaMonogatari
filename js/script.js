9518b6b1c3d40871bfa8631f7f3be2afe5668f03
ゆーたろー <1029rie.u.chan@gmail.com>
2026-02-16 03:25:32 +0900

    css追加

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
-const messageElement = document.querySelector('.js-message');
-const toggleButton = document.querySelector('.js-toggle-button');
-
-if (messageElement && toggleButton) {
-  toggleButton.addEventListener('click', () => {
-    messageElement.textContent =
-      messageElement.textContent === '準備中です。'
-        ? 'ベースファイルの作成が完了しました。'
-        : '準備中です。';
-  });
-}
+const slotNumbers = document.querySelectorAll('.js-slot-number');
